5ee3e6494d827a0e027e3fdb8dfc2e4b2eca2437
Hidetatsu Yaginuma <ygnmhdtt@gmail.com>
2018-11-26 23:04:03 +0900

    add test

---
+++ gjson_test.go
@@ -0,0 +1,42 @@
+package gjson
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+func TestDecode(t *testing.T) {
+	for i, testcase := range []struct {
+		invalid bool
+		in      string
+	}{
+		{in: `{}`},
+		{in: `  {}`},
+		{in: `{  }`},
+		{in: `{}  `},
+		{in: `  {  }  `},
+		{in: `{}  1`, invalid: true},
+	} {
+		expected := make(map[string]interface{})
+		err1 := json.Unmarshal([]byte(testcase.in), &expected)
+		out, err2 := Decode([]byte(testcase.in))
+		if testcase.invalid {
+			if err1 == nil || err2 == nil {
+				t.Errorf("error is expected: err1: %v, err2: %v", err1, err2)
+			}
+			continue
+		}
+
+		if err1 != nil {
+			t.Errorf("err1 is not nil: %v", err1)
+		}
+
+		if err2 != nil {
+			t.Errorf("err2 is not nil: %v", err2)
+		}
+		if !reflect.DeepEqual(out, expected) {
+			t.Errorf("#%d: %v, want %v", i, out, expected)
+		}
+	}
+}
